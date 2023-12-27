a08c37db190ac1431be0e4fe8d2bdcab9c419869
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2023-12-27 11:08:37 +0700

    step 1 for loop completed

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
+// 'FOR' CODE
 const words = ["Cachalot", "Pétunia", "Serviette"];
 let score = 0;
 
-{
-  let userWord = prompt("Enter the word: " + words[0]);
-console.log(userWord);
+for (let i = 0; i < words.length; i++) {
+  userWord = prompt(`Type the word: ${words[i]}`);
+  if (userWord === words[i]) {
+    score++;
+  }
+}
+console.log(score);
 
-if (userWord === words[0]) {
-  score +=1
-  console.log("Well done!");
-} else {
-  console.log("Sorry, there must be a typo.");
-}
-}
-{
-userWord = prompt("Enter the word: " + words[1]);
-console.log(userWord);
+//-----------------------------
+// 'WHILE' CODE
 
-if (userWord === words[1]) {
-  score +=1
-  console.log("Well done!");
-} else {
-  console.log("Sorry, there must be a typo.");
-}
-}
-{
-userWord = prompt("Enter the word: " + words[2]);
-console.log(userWord);
+// const words = ["Cachalot", "Pétunia", "Serviette"];
+// let score = 0;
 
-if (userWord === words[2]) {
-  score +=1
-  console.log("Well done!");
-} else {
-  console.log("Sorry, there must be a typo.");
-}
-}
+// let i = 0;
+// while (i < words.length) {
+//   userWord = prompt(`Enter the word: ${words[i]}`);
+//   if (userWord === words[i]) {
+//   console.log(userWord);
+//   score++;
+//   console.log(score);
+//   i++;
+//   } else {
+//     i++;
+//   }
+// };
+
+// -----------------------------------
+// MANUAL CODE
+
+// const words = ["Cachalot", "Pétunia", "Serviette"];
+// let score = 0;
+
+// {
+//   let userWord = prompt("Enter the word: " + words[0]);
+// console.log(userWord);
+
+// if (userWord === words[0]) {
+//   score +=1
+//   console.log("Well done!");
+// } else {
+//   console.log("Sorry, there must be a typo.");
+// }
+// }
+// {
+// userWord = prompt("Enter the word: " + words[1]);
+// console.log(userWord);
+
+// if (userWord === words[1]) {
+//   score +=1
+//   console.log("Well done!");
+// } else {
+//   console.log("Sorry, there must be a typo.");
+// }
+// }
+// {
+// userWord = prompt("Enter the word: " + words[2]);
+// console.log(userWord);
+
+// if (userWord === words[2]) {
+//   score +=1
+//   console.log("Well done!");
+// } else {
+//   console.log("Sorry, there must be a typo.");
+// }
+// }
+
+// -----------------------------
+
+// 'FOR' loop
+// for (let i = 0; i < 3; i++){
+//   console.log(`hello ${i}`);
+// }
+
+// 'WHILE' loop
