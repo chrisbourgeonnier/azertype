439c247a6bb5bd75316ff1cc4ac1385d15d8293e
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2023-12-27 11:43:56 +0700

    completed the loop part (3rd part)

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-// 'FOR' CODE
-const words = ["Cachalot", "Pétunia", "Serviette"];
+const words = ["Whale", "Petunia", "Towel"];
+const phrases = ["Don't Panic!", "Life, the Universe and Everything", "So long, and thanks for all the fish"];
 let score = 0;
 
-for (let i = 0; i < words.length; i++) {
-  userWord = prompt(`Type the word: ${words[i]}`);
-  if (userWord === words[i]) {
-    score++;
+let choice = ["words", "phrases"];
+userChoice = prompt("Play with 'words' or 'phrases'")
+
+while (userChoice !== choice[0] && userChoice !== choice[1]) {
+  userChoice = prompt("Play with 'words' or 'phrases'")
+};
+
+if (userChoice === choice[0]) {
+  for (let i = 0; i < words.length; i++) {
+    userWord = prompt(`Type the word: ${words[i]}`);
+    if (userWord === words[i]) {
+      score++;
+    }
   }
+  console.log(`Your score is ${score} out of ${words.length}`);
+} else {
+  for (let i = 0; i < phrases.length; i++) {
+    userPhrase = prompt(`Type the phrase: ${phrases[i]}`);
+    if (userPhrase === phrases[i]) {
+      score++;
+    }
+  }
+  console.log(`Your score is ${score} out of ${phrases.length}`);
 }
-console.log(score);
+
+
+// ---------------------------------
+// 'FOR' CODE
+// const words = ["Whale", "Petunia", "Towel"];
+// const phrases = ["Don't Panic!", "Life, the Universe and Everything", "So long, and thanks for all the fish"];
+// let score = 0;
+
+// for (let i = 0; i < words.length; i++) {
+//   userWord = prompt(`Type the word: ${words[i]}`);
+//   if (userWord === words[i]) {
+//     score++;
+//   }
+// }
+// console.log(score);
 
 //-----------------------------
 // 'WHILE' CODE
